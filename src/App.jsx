666a75ce790f50a6957601c1333eea1824f78337
Alexhong1269 <Alexhong1269@gmail.com>
2024-02-08 12:09:03 -0500

    moved files from wook to mine

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import "./App.css";
+
+const StyledApp = styled.div``;
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const [data, setData] = react.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      //fetching data from the json folder that connceted to index.cjs
-      .then((data) => setData(data.message));
-  }, []);
-
-  //theres suppose to be a display return but im not writting all that
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <StyledApp className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </StyledApp>
+  );
 }
 
-export default App
+export default App;
